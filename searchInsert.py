f9d3aff5c29ab0c38a51cf6663828df11ed02617
lookastar <lookastar@gmail.com>
2022-04-08 20:35:19 +0200

    Add 35. Search Insert Position

---
+++ searchInsert.py
@@ -0,0 +1,30 @@
+from math import floor
+
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        """
+        :type nums: List[int], target: int
+        :rtype: int
+        """
+        a=0
+        b=len(nums)-1
+
+        while b>=a:
+            center=floor((a+b)/2)
+            
+            if target==nums[center]:
+                return center
+
+            if a==b:
+                if target>nums[center]:
+                    return center+1
+                else:
+                    return center
+
+            if target>nums[center]:
+                a=center+1
+            else:
+                if b==a+1:
+                    return a
+                b=center-1
