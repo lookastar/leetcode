dba3d7b4d00b8d8bae1aca5c18fec7a031f90cf5
lookastar <lookastar@gmail.com>
2022-05-16 07:32:08 +0200

    Add 704. Binary Search

---
+++ 704. Binary Search.py
@@ -0,0 +1,17 @@
+from types import List
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums) -1
+
+        while left <= right:
+            center = left + (right-left)//2
+            if(nums[center] == target):
+                return center
+            elif nums[center] > target:
+                right = center -1
+            else:
+                left = center +1
+
+        return -1
